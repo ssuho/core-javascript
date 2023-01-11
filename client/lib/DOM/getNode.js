0a24f7fda8e13e23ca3809f358dafeafa72930c0
Suho Kim <devsuholim@gmail.com>
2023-01-11 09:12:27 +0900

    [추가] error 유틸 함수 만들기

--- client/lib/DOM/getNode.js
+++ client/lib/DOM/getNode.js
@@ -3,6 +3,8 @@ function getNode(node){
   if(typeof node !== 'string'){
     throw new Error('getNode 함수의 인자는 문자 타입 이여야 합니다.');
   }
+
+  // if(!isString(node)) typeError('에러가 발생했습니다.');
   
   return document.querySelector(node)
 }
@@ -15,4 +17,3 @@ function getNodes(node){
   
   return document.querySelectorAll(node)
 }
-
