1f14eae00d7ab83c42ea8254f682f5d0f0da7491
Suho Kim <devsuholim@gmail.com>
2023-01-13 09:18:06 +0900

    bubble.js 생성

---
+++ client/chapter/DOM/06.event.js
@@ -0,0 +1,68 @@
+/* ---------------------------------------------------------------------- */
+/* Event Handling                                                         */
+/* ---------------------------------------------------------------------- */
+
+/* 이벤트 핸들링 3가지 방법 --------------------------------------------------- */
+
+// 1. HTML 속성 : onclick="handler()"
+// 2. DOM 프로퍼티 : element.onclick = handler
+// 3. 메서드 : element.addEventListener(event, handler[, phase])
+
+/* 이벤트 추가/제거 --------------------------------------------------------- */
+
+const first = getNode('.first');
+const second = getNode('.second');
+const ground = getNode('.ground');
+const ball = getNode('.ball');
+
+
+
+function handler(){
+  console.log('hit !');
+
+  // css('.second','display','none')
+
+
+}
+
+// first.addEventListener('click', handler);
+
+
+
+const off = bindEvent('.first','click',handler);
+
+bindEvent('.second','click',off);
+
+
+
+
+// second.addEventListener('click',function(){
+//   first.removeEventListener('click', handler);
+// })
+
+
+
+ground.addEventListener('mousemove', function(e){
+  console.log(e.offsetX, e.offsetY)
+
+  ball.style.transform = `translate(${e.offsetX}px, ${e.offsetY}px)`;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// - addEventListener
+// - removeEventListener
---
+++ client/chapter/DOM/07.bubble.js
@@ -0,0 +1,24 @@
+/* ---------------------------------------------------------------------- */
+/* Event bubbling & capturing                                             */
+/* ---------------------------------------------------------------------- */
+
+
+/* 버블링 ----------------------------------------------------------------- */
+
+const visual = getNode('.visual')
+const news = getNode('.news');
+const desc = getNode('.desc');
+
+visual.addEventListener('click', function(){
+  console.log('%c visual', 'background:dodgerblue')})
+
+news.addEventListener('click', function(){
+  console.log('%c news', 'background:orange')
+})
+
+desc.addEventListener('click', function(e){
+  e.stopPropagation();
+  console.log('%c desc', 'background:hotpink')
+})
+
+/* 캡처링 ----------------------------------------------------------------- */
